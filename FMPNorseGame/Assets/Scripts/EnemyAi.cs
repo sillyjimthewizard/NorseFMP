6faf04a564de61366f1dec8998ba76c99a87dd62
sillyjimthewizard <cha24001426@heartofyorkshire.ac.uk>
2026-03-17 12:06:18 +0000

    Enemy Layout

--- FMPNorseGame/Assets/Scripts/EnemyAi.cs
+++ FMPNorseGame/Assets/Scripts/EnemyAi.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Splines;
 
 public class EnemyAi : MonoBehaviour
 {
 
-    public SplineContainer Path;
-    public 
+    public NavMeshAgent agent;
+    public Vector3 moveTo;
+    public GameObject TheBoss;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        TheBoss = GameObject.Find("TheBoss");
+
+        moveTo = new Vector3(TheBoss.transform.position.x, TheBoss.transform.position.y, TheBoss.transform.position.z);
+
+        agent.SetDestination(moveTo);
     }
 
     // Update is called once per frame
