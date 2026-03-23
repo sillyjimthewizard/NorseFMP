74778ab2a5cbebc65a3c9327871e5e55189141e0
sillyjimthewizard <cha24001426@heartofyorkshire.ac.uk>
2026-03-23 14:20:14 +0000

    numbers go up

--- FMPNorseGame/Assets/Scripts/EnemyAi.cs
+++ FMPNorseGame/Assets/Scripts/EnemyAi.cs
@@ -8,6 +8,10 @@ public class EnemyAi : MonoBehaviour
     public NavMeshAgent agent;
     public Vector3 moveTo;
     public GameObject TheBoss;
+    
+
+    public float DistanceToBoss;
+    public float InitialDistance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +22,37 @@ public class EnemyAi : MonoBehaviour
         moveTo = new Vector3(TheBoss.transform.position.x, TheBoss.transform.position.y, TheBoss.transform.position.z);
 
         agent.SetDestination(moveTo);
+        DistanceToBoss = Vector3.Distance(this.transform.position, TheBoss.transform.position);
+        InitialDistance = DistanceToBoss;
     }
 
     // Update is called once per frame
-    void Update()
+    /* void OnCollisionEnter(Collision other)
+     {
+         if (other.gameObject.CompareTag("TheBoss"))
+         {
+             Debug.Log("bosshit");
+         }
+     }
+
+     */
+
+
+    public void Update()
     {
-        
+       DistanceToBoss = Vector3.Distance(this.transform.position, TheBoss.transform.position);
+
+        if (DistanceToBoss <= 2)
+        {
+            
+            Destroy(this.gameObject);
+        }
     }
+
+    public void OnDestroy()
+    {
+       GameManager.Instance.Currency += InitialDistance - DistanceToBoss;
+    }
+
+    
 }
---
+++ FMPNorseGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [Header("TickSystem")]
+    public float TickTimer ;
+    public float SetTickTime;
+    public float GlobalTime;
+
+    [Header("SpawnSystem")]
+    public GameObject[] SpawnPoints;
+    public GameObject Unit;
+    public int SpawnCounter;
+    public int SpawnNumber;
+    public int SpawnPointNum;
+
+    [Header("MONEY")]
+    public float Currency;
+
+    public static GameManager Instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        Instance = this;
+        //SetTickTime = TickTimer;
+        SetSpawns();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (TickTimer >= 0)
+        {
+            TickTimer -= Time.deltaTime;
+            GlobalTime += Time.deltaTime;
+        }
+        else if (TickTimer <= 0)
+        {
+            SpawnCounter++;
+            TickTimer = SetTickTime;
+        }
+
+        SpawnEnemy();
+
+        
+    }
+
+    public void SpawnEnemy()
+    {
+        if (SpawnCounter == SpawnNumber)
+        {
+            Instantiate(Unit, SpawnPoints[SpawnPointNum].gameObject.transform);
+            SpawnCounter = 0;
+        }
+    }
+    public void SetSpawns()
+    {
+        SpawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoints");
+    }
+
+
+
+}
